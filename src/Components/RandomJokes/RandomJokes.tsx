61838b987650eae00a2c5feee9a8b758c15e1605
KillReal63 <104447337+KillReal63@users.noreply.github.com>
2023-12-08 15:44:38 +0400

    add styles

--- src/Components/RandomJokes/RandomJokes.tsx
+++ src/Components/RandomJokes/RandomJokes.tsx
@@ -2,9 +2,9 @@ import { FC } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../Services/Slices/store';
 import { fetchJoke } from '../../api/jokeApi';
+import { Jokes, addJoke } from '../../Services/Slices/Jokes/jokes';
 import JokeItem from '../JokeItem/JokeItem';
 import Button from '../../Ui/Button/Button';
-import { Jokes, addJoke } from '../../Services/Slices/Jokes/jokes';
 import './RandomJokes.css';
 
 const url = 'https://official-joke-api.appspot.com/jokes/programming/ten';
@@ -19,18 +19,20 @@ const RandomJokes: FC<Jokes> = ({ jokes }) => {
   };
 
   return (
-    <div className='random-jokes-container'>
+    <div>
       <h1 style={{ marginBottom: 5 }}>Random jokes</h1>
-      {jokes.loading && <div>Загрузка</div>}
-      {jokes.loading ||
-        jokes.data.map((item, index) => (
-          <JokeItem
-            jokes={item}
-            key={index}
-            control={<Button onClick={() => onAdd(item.id)}>Add</Button>}
-          />
-        ))}
-      {jokes.loading || <Button onClick={onClick}>Update jokes</Button>}
+      <div className='random-jokes-container custom_scroll'>
+        {jokes.loading && <div>Загрузка</div>}
+        {jokes.loading ||
+          jokes.data.map((item, index) => (
+            <JokeItem
+              jokes={item}
+              key={index}
+              control={<Button onClick={() => onAdd(item.id)}>Add</Button>}
+            />
+          ))}
+        {jokes.loading || <Button onClick={onClick}>Update jokes</Button>}
+      </div>
     </div>
   );
 };
--- src/Components/UserJokes/UserJokes.tsx
+++ src/Components/UserJokes/UserJokes.tsx
@@ -19,26 +19,53 @@ const UserJokes: FC<Jokes> = ({ jokes }) => {
   return (
     <div>
       <h1 style={{ marginBottom: 5 }}>Favorite jokes</h1>
-      <div className='user-jokes'>
+      <div className='user-jokes custom_scroll'>
         {jokes.userJokes.map((item, index) => (
           <JokeItem
             jokes={item}
             key={index}
             control={
-              <div>
+              <div className='control-panel'>
                 <Button onClick={() => dispatch(removeJoke(item.id))}>
                   Delete
                 </Button>
-                <Button onClick={() => dispatch(likeJoke(item.id))}>
-                  Like: {item.likes}
-                </Button>
-                <Button
-                  onClick={() => {
-                    dispatch(dislikeJoke(item.id));
-                  }}
-                >
-                  Dislike
-                </Button>
+                <span style={{ fontWeight: 'bold' }}>Likes: {item.likes}</span>
+                <div className='counter'>
+                  <Button onClick={() => dispatch(likeJoke(item.id))}>
+                    <svg
+                      width='20px'
+                      height='20px'
+                      viewBox='0 0 48 48'
+                      version='1'
+                      xmlns='http://www.w3.org/2000/svg'
+                      enable-background='new 0 0 48 48'
+                    >
+                      <path
+                        fill='#F44336'
+                        d='M34,9c-4.2,0-7.9,2.1-10,5.4C21.9,11.1,18.2,9,14,9C7.4,9,2,14.4,2,21c0,11.9,22,24,22,24s22-12,22-24 C46,14.4,40.6,9,34,9z'
+                      />
+                    </svg>
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      dispatch(dislikeJoke(item.id));
+                    }}
+                  >
+                    <svg
+                      width='20px'
+                      height='20px'
+                      viewBox='0 0 48 48'
+                      version='1'
+                      xmlns='http://www.w3.org/2000/svg'
+                      enable-background='new 0 0 48 48'
+                    >
+                      <path
+                        fill='#000000'
+                        d='M34,9c-4.2,0-7.9,2.1-10,5.4C21.9,11.1,18.2,9,14,9C7.4,9,2,14.4,2,21c0,11.9,22,24,22,24s22-12,22-24 C46,14.4,40.6,9,34,9z'
+                      />
+                    </svg>
+                  </Button>
+                </div>
               </div>
             }
           />
